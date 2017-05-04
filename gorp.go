125a7802aab1b00d26fab7676c60885336d2a430
shane F <shane@jobrapp.com>
2017-05-04 14:18:46 -0700

    make gorp not shit the bed when columns are missing

--- gorp.go
+++ gorp.go
@@ -270,12 +270,7 @@ func columnToFieldIndex(m *DbMap, t reflect.Type, name string, cols []string) ([
 			missingColNames = append(missingColNames, colName)
 		}
 	}
-	if len(missingColNames) > 0 {
-		return colToFieldIndex, &NoFieldInTypeError{
-			TypeName:        t.Name(),
-			MissingColNames: missingColNames,
-		}
-	}
+
 	return colToFieldIndex, nil
 }
 
